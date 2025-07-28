eb6908b39b8c31594afc4e3dfd23d09c11a680a5
ubuntu <foenix208@gmail.com>
2025-07-29 01:46:22 +0200

    erreur loaderCommandes.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -3,12 +3,11 @@ const Discord = require("discord.js")
 module.exports = {
     
     name : "ping", 
-    description : "affiche la latence",
+    description : "affiche la latence.",
     permissions : "Aucune",
     dm :  true,
 
-
-    async run(bot , messsage){
+    async run(bot , messsage, argv){
         await messsage.reply(`Pong : \`${bot.ws.ping}\``)
     }
 }
